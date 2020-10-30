ab1f624ecc0b897c2015329eeb8e0db47206a43b
Evan Lacy <lacyevan@yahoo.com>
2020-10-30 00:05:28 -0400

    Second Commit, remade projects

---
+++ Cafe_Repo/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe_Repo
+{
+    public class Menu
+    {
+
+        //Create new menu items
+
+        //Read
+        //Get all menu items
+        //Get menu items by number
+        //Get menu items by name
+        //Get meal ingredients
+
+        //Update
+        //not done yet
+
+        //Delete
+        //Remove a menu item by name
+    }
+}
---
+++ Cafe_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cafe_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Cafe_Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("73f0f7f4-761f-427a-8000-d464493596f0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
